8561ed54f9980ab9ec4246a78e18e291e07093d5
jsnjfndsfdsasd <40805903+ujhsfjhfsfa@users.noreply.github.com>
2019-02-24 03:04:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const ytdl = require('ytdl-core');
 const fs = require('fs');
 const gif = require("gif-search");
 const prefix = "3"
-const adminprefix = "3"
+const adminprefix = "c"
 /////////////////////////
 ////////////////////////
 //////////////////////
